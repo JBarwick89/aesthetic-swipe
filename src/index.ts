68837391ed3641e4dd97b2ba9b95912398a7d820
Josh Barwick <j.Barwick89@gmail.com>
2026-03-04 21:29:14 -0600

    add error handlers

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,20 @@ import { OpenAPIHono } from '@hono/zod-openapi'
 const app = new OpenAPIHono()
 
 app.get('/', (c) => {
-  return c.text('Aesthetic Swipe CMOP')
+  return c.text('Aesthetic Swipe')
+})
+
+app.get('/error', (c) => {
+  throw new Error('This is a test error')
 })
 
 app.notFound(c => c.json({
   message: `'The requested resource was not found' - ${c.req.path}`,
 }, 404))
+
+app.onError((err, c) => {
+  console.error(`Error occurred while handling request to ${c.req.path}:`, err)
+  return c.json({ message: 'Internal Server Error' }, 500)
+})
 
 export default app
